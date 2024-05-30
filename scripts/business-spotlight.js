0c3b1fea400f2563e488e579a6e77db3f57ef2e8
reginmund9 <moronitaca@gmail.com>
2024-05-29 23:15:13 -0600

    again

--- scripts/business-spotlight.js
+++ scripts/business-spotlight.js
@@ -6,9 +6,12 @@ const requestURL = '../data/members.json';
  * 'name' - holds the name of the company.
  * 'name_add' - holds an additional title for the company. 
  * 'address' - holds the address of the company.
+ * 'address_end' - contains the city, state, and zipcode of the address.
+ * 'phone' - holds the company's phone number.
+ * 'url' - holds the company's web address.
  * 'logo' - contains a 300x200px image of the company's logo.
  * 'mem_lvl' - holds the current membership level of the company. 
-*/
+ */
 
 const spotlight1 = document.querySelector('#spotlight-1');
 const spotlight2 = document.querySelector('#spotlight-2');
@@ -19,9 +22,9 @@ let npList = [];
 let bronzeList = [];
 let silverGoldList = [];
 let spotlightList = [];
-var choice1 = {name: '', name_add: '', address: '', logo:'images/placeholder-300x200.webp', mem_lvl: 'NP'};
-var choice2 = {name: '', name_add: '', address: '', logo:'images/placeholder-300x200.webp', mem_lvl: 'NP'};
-var choice3 = {name: '', name_add: '', address: '', logo:'images/placeholder-300x200.webp', mem_lvl: 'NP'};
+var choice1 = {name: '', name_add: '', address: '', address_end: '', phone:'', url:'', logo:'../images/placeholder/placeholder-300x200.webp', mem_lvl: 'NP'};
+var choice2 = {name: '', name_add: '', address: '', address_end: '', phone:'', url:'', logo:'../images/placeholder/placeholder-300x200.webp', mem_lvl: 'NP'};
+var choice3 = {name: '', name_add: '', address: '', address_end: '', phone:'', url:'', logo:'../images/placeholder/placeholder-300x200.webp', mem_lvl: 'NP'};
 
 fetch(requestURL)
   .then(function (response) { // returns a Promise which will be used as an argument.
@@ -35,7 +38,7 @@ fetch(requestURL)
     displaySpotlights(choice1, choice2, choice3);
   });
 
-function sortMembers(business) {
+  function sortMembers(business) {
     // Sort each business into the corresponding list of memberships.
     let membership = business.mem_lvl;
     switch (membership) {
@@ -51,9 +54,9 @@ function sortMembers(business) {
             silverGoldList.push(business);
             break;
     }
-}
+  }
 
-function selectThreeSpotlights(list) {
+  function selectThreeSpotlights(list) {
     // Select 3 random businesses to feature from the Gold Members list.
     let currIndex;
     let prevIndex;
@@ -89,62 +92,73 @@ function selectThreeSpotlights(list) {
         choice2 = spotlightList[1];
         choice3 = spotlightList[2];
     }
-}
+  }
 
-function randIndex(listLength) {
+  function randIndex(listLength) {
     return Math.floor(Math.random() * listLength);
-}
+  }
 
-function displaySpotlights(choice1, choice2, choice3) {
+  function displaySpotlights(choice1, choice2, choice3) {
     // Create elements for first spotlight.
     let image1 = document.createElement('img');
     let title1 = document.createElement('h4');
     let par1 = document.createElement('p');
+    let link1 = document.createElement('a');
     // Create elements for second spotlight.
     let image2 = document.createElement('img');
     let title2 = document.createElement('h4');
     let par2 = document.createElement('p');
+    let link2 = document.createElement('a');
     // Create elements for third spotlight.
     let image3 = document.createElement('img');
     let title3 = document.createElement('h4');
     let par3 = document.createElement('p');
+    let link3 = document.createElement('a');
 
     // Change the textContent property of the h2 element to contain the prophet's full name
     title1.textContent = choice1.name;
-    par1.textContent = choice1.address;
+    par1.textContent = choice1.address + ', ' + choice1.address_end;
+    link1.textContent = choice1.url;
 
     title2.textContent = choice2.name;
-    par2.textContent = choice2.address;
+    par2.textContent = choice2.address + ', ' + choice2.address_end;
+    link2.textContent = choice2.url;
 
     title3.textContent = choice3.name;
-    par3.textContent = choice3.address;
+    par3.textContent = choice3.address + ', ' + choice3.address_end;
+    link3.textContent = choice3.url;
+
+    // Build the href attribute for url links.
+    link1.setAttribute('href', choice1.url);
+    link2.setAttribute('href', choice2.url);
+    link3.setAttribute('href', choice3.url);
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values.
     image1.setAttribute('src', choice1.logo);
     image1.setAttribute('alt', 'Logo of ' + choice1.name);
-    image1.setAttribute('width', '300');
-    image1.setAttribute('height', '200');
+    image1.setAttribute('width', '300'); image1.setAttribute('height', '200');
 
     image2.setAttribute('src', choice2.logo);
     image2.setAttribute('alt', 'Logo of ' + choice2.name);
-    image2.setAttribute('width', '300');
-    image2.setAttribute('height', '200');
+    image2.setAttribute('width', '300'); image1.setAttribute('height', '200');
 
     image3.setAttribute('src', choice3.logo);
     image3.setAttribute('alt', 'Logo of ' + choice3.name);
-    image3.setAttribute('width', '300');
-    image3.setAttribute('height', '200');
+    image3.setAttribute('width', '300'); image1.setAttribute('height', '200');
 
     // Add/append the section with the created child elements.
     spotlight1.appendChild(title1);
     spotlight1.appendChild(image1);
+    spotlight1.appendChild(link1);
     spotlight1.appendChild(par1);
 
     spotlight2.appendChild(title2);
     spotlight2.appendChild(image2);
+    spotlight2.appendChild(link2);
     spotlight2.appendChild(par2);
 
     spotlight3.appendChild(title3);
     spotlight3.appendChild(image3);
+    spotlight3.appendChild(link3);
     spotlight3.appendChild(par3);
-}
+  }
